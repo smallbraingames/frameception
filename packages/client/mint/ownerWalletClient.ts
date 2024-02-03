524f1acbb27d386da74a106fdf989999ed519a44
smallbraingames <smallbraindev@gmail.com>
2024-02-03 13:06:03 -0800

    feat(client): public/wallet clients, chain switching

---
+++ packages/client/mint/ownerWalletClient.ts
@@ -0,0 +1,18 @@
+import { Hex, createWalletClient, http } from 'viem';
+import { privateKeyToAccount } from 'viem/accounts';
+import chain from './chain';
+
+const ownerWalletPrivateKey = process.env.OWNER_WALLET_PRIVATE_KEY as
+  | Hex
+  | undefined;
+if (!ownerWalletPrivateKey) {
+  throw new Error('[OwnerWallet] OWNER_WALLET_PRIVATE_KEY is not set');
+}
+const privateKeyAccount = privateKeyToAccount(ownerWalletPrivateKey);
+
+const ownerWalletClient = createWalletClient({
+  account: privateKeyAccount,
+  chain: chain,
+  transport: http(),
+});
+export default ownerWalletClient;
