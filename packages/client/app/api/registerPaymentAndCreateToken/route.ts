e082548d2c1faf7bee9ad9bdc21c68b4959dfad8
smallbraingames <smallbraindev@gmail.com>
2024-02-03 20:03:43 -0800

    feat(client): update create function to match new contract impl.

--- packages/client/app/api/registerPaymentAndCreateToken/route.ts
+++ packages/client/app/api/registerPaymentAndCreateToken/route.ts
@@ -1,22 +1,13 @@
-import chain from '@/mint/chain';
-import create from '@/mint/create';
-import { getReadCollectionContract } from '@/mint/getCollectionContract';
 import getCreateReceiptId from '@/mint/getCreateReceiptId';
 import getTokenImageKVKey from '@/mint/getTokenImageKVKey';
-import ownerWalletClient from '@/mint/ownerWalletClient';
-import publicClient from '@/mint/publicClient';
+import getTransactionReceipt from '@/mint/getTransactionReceipt';
 import { kv } from '@vercel/kv';
 import { NextRequest, NextResponse } from 'next/server';
-import { Hex, TransactionReceipt, getAddress } from 'viem';
-
-const MAX_SUPPLY = 1e6;
-
-const getKey = (chainId: number, hash: Hex) => `${chainId}:${hash}`;
+import { Hex } from 'viem';
 
 const getResponse = async (req: NextRequest): Promise<NextResponse> => {
   const body = await req.json();
   const hash = body.hash as Hex | undefined;
-  const supply = body.supply as number | undefined;
   const url = body.url as string | undefined;
 
   if (!url) {
@@ -27,124 +18,32 @@ const getResponse = async (req: NextRequest): Promise<NextResponse> => {
     return NextResponse.json({ message: 'Invalid hash' }, { status: 500 });
   }
 
-  if (
-    !supply ||
-    supply <= 0 ||
-    supply > MAX_SUPPLY ||
-    !Number.isInteger(supply)
-  ) {
-    return NextResponse.json({ message: 'Invalid supply' }, { status: 500 });
-  }
-
-  const transaction = await publicClient.getTransaction({
-    hash,
-  });
-
-  const chainId = chain.id;
-  const sender = transaction.from;
-  const value = transaction.value;
-  if (!transaction.to) {
-    return NextResponse.json(
-      { message: 'Invalid transaction: No to' },
-      { status: 500 }
-    );
-  }
-  const to = getAddress(transaction.to);
-  const ownerAddress = getAddress(ownerWalletClient.account.address);
-
-  if (to !== ownerAddress) {
-    return NextResponse.json(
-      { message: 'Invalid transaction: Not to owner' },
-      { status: 500 }
-    );
-  }
-
-  if (value <= 0) {
-    return NextResponse.json(
-      { message: 'Invalid transaction: Zero value' },
-      { status: 500 }
-    );
-  }
-
-  const key = getKey(chainId, hash);
-  const hasUsedPayment = await kv.get(key);
-  if (hasUsedPayment) {
-    return NextResponse.json(
-      { message: 'Invalid transaction: Already used hash' },
-      { status: 500 }
-    );
-  }
-
-  await kv.set(key, true);
-
-  let receipt: TransactionReceipt;
-  try {
-    receipt = await create(sender, supply);
-  } catch (e) {
+  const transactionReceipt = await getTransactionReceipt(hash);
+  if (!transactionReceipt) {
     console.error(
-      '[RegisterPaymentAndCreateToken] Error creating token, refunding user',
-      e
+      `[RegisterPaymentAndCreateToken] No receipt from hash: ${hash}`
     );
-    try {
-      await ownerWalletClient.sendTransaction({
-        to: sender,
-        value: value,
-      });
-    } catch (e) {
-      console.error(
-        `[RegisterPaymentAndCreateToken] Error refunding user: (sender) ${sender}, (to) ${to}, (value) ${value}, (hash) ${hash}, (chainId) ${chainId}`,
-        e
-      );
-      return NextResponse.json(
-        { message: 'Invalid transaction: Already used hash' },
-        { status: 500 }
-      );
-    }
     return NextResponse.json(
-      { message: 'Error creating token, refunded' },
+      { message: 'Invalid transaction' },
       { status: 500 }
     );
   }
 
-  console.log(
-    `[RegisterPaymentAndCreateToken] Created token: (sender) ${sender}, (to) ${to}, (value) ${value}, (hash) ${hash}, (chainId) ${chainId}`
-  );
+  const tokenId = getCreateReceiptId(transactionReceipt);
 
-  let id = getCreateReceiptId(receipt);
-  if (!id) {
+  if (!tokenId) {
     console.error(
-      `[RegisterPaymentAndCreateToken] Error getting token id: (sender) ${sender}, (to) ${to}, (value) ${value}, (hash) ${hash}, (chainId) ${chainId}, getting most recent id from contract`
+      `[RegisterPaymentAndCreateToken] No tokenId from receipt: ${JSON.stringify(
+        transactionReceipt
+      )}`
     );
 
-    try {
-      const contract = getReadCollectionContract(publicClient);
-      id = Number(await contract.read.lastId());
-    } catch (e) {
-      console.error(
-        `[RegisterPaymentAndCreateToken] Error getting most recent id: (sender) ${sender}, (to) ${to}, (value) ${value}, (hash) ${hash}, (chainId) ${chainId}`,
-        e
-      );
-      return NextResponse.json(
-        { message: 'Created collection, but error getting token id' },
-        { status: 500 }
-      );
-    }
+    return NextResponse.json({ message: 'Invalid receipt' }, { status: 500 });
   }
 
-  try {
-    await kv.set(getTokenImageKVKey(id), url);
-  } catch (e) {
-    console.error(
-      `[RegisterPaymentAndCreateToken] Error setting token image: (id) ${id}, (url) ${url}`,
-      e
-    );
-    return NextResponse.json(
-      { message: 'Created collection, but error setting token image' },
-      { status: 500 }
-    );
-  }
+  await kv.set(getTokenImageKVKey(tokenId), url);
 
-  return NextResponse.json({ message: 'Created', id }, { status: 200 });
+  return NextResponse.json({ message: 'Created' }, { status: 200 });
 };
 
 export const POST = async (req: NextRequest): Promise<Response> => {
