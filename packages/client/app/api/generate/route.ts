255a2de85bbc080ddf51404032943e29a2b45878
smallbraingames <smallbraindev@gmail.com>
2024-02-03 18:10:32 -0800

    feat(client): pass url to redirect

--- packages/client/app/api/generate/route.ts
+++ packages/client/app/api/generate/route.ts
@@ -1,4 +1,3 @@
-import { generateNewImage } from '@/generate/generateNewImage';
 import getImageUrl from '@/generate/getImageUrl';
 import { FrameRequest, getFrameHtmlResponse } from '@coinbase/onchainkit';
 import { kv } from '@vercel/kv';
@@ -14,29 +13,39 @@ const getResponse = async (req: NextRequest): Promise<NextResponse> => {
 
   const body: FrameRequest = await req.json();
 
-  let url;
-  if (body?.untrustedData?.inputText) {
-    text = body.untrustedData.inputText;
-    console.log('[Generate] Text: ', text);
-    const prevPromptKey = getPreviousPromptKey(body.untrustedData.fid);
-    if (text === '') {
-      const prevPrompt = await kv.get<string>(prevPromptKey);
-      url = await getImageUrl(prevPrompt ?? '');
-    } else {
-      [, url] = await Promise.all([
-        getImageUrl(text),
-        kv.set(prevPromptKey, text),
-      ]);
-    }
-  }
+  buttonIndex = body.untrustedData.buttonIndex;
+
+  let url: string | undefined;
 
   if (body?.untrustedData?.buttonIndex) {
     buttonIndex = body.untrustedData.buttonIndex;
-    console.log('buttonIndex: ', buttonIndex);
 
-    if (buttonIndex === 3) {
-      // TODO: redirect to NFT creation page
-      return NextResponse.redirect(NEXT_PUBLIC_URL, {
+    if (buttonIndex === 1) {
+      text = body.untrustedData.inputText;
+      console.log('[Generate] Text: ', text);
+      const prevPromptKey = getPreviousPromptKey(body.untrustedData.fid);
+      if (text === '') {
+        const prevPrompt = await kv.get<string>(prevPromptKey);
+        url = await getImageUrl(prevPrompt ?? '');
+      } else {
+        [, url] = await Promise.all([
+          kv.set(prevPromptKey, text),
+          getImageUrl(text),
+        ]);
+      }
+    }
+
+    if (buttonIndex === 2) {
+      const prevPromptKey = getPreviousPromptKey(body.untrustedData.fid);
+      const prevPrompt = await kv.get<string>(prevPromptKey);
+      if (!prevPrompt) {
+        return NextResponse.json({
+          status: 500,
+          message: 'No previous prompt',
+        });
+      }
+      const url = await getImageUrl(prevPrompt);
+      return NextResponse.redirect(`${NEXT_PUBLIC_URL}/create?url=${url}`, {
         status: 302,
       });
     }
@@ -46,7 +55,7 @@ const getResponse = async (req: NextRequest): Promise<NextResponse> => {
     getFrameHtmlResponse({
       buttons: [
         {
-          label: "Refresh",
+          label: 'Refresh',
         },
         {
           label: `Create NFT: ${buttonIndex}`,
@@ -54,7 +63,7 @@ const getResponse = async (req: NextRequest): Promise<NextResponse> => {
         },
       ],
       input: {
-        text: 'Write your prompt here',
+        text: 'Write a new prompt, or leave blank to refresh the previous prompt',
       },
       image: url ?? '',
       post_url: `${NEXT_PUBLIC_URL}/api/generate`,
