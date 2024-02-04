3e14f9e26e69b5bfb60f0b150e009f753050d61c
smallbraingames <smallbraindev@gmail.com>
2024-02-03 19:32:03 -0800

    feat(contracts): accept payment on contract

--- packages/contracts/test/Base.t.sol
+++ packages/contracts/test/Base.t.sol
@@ -10,7 +10,7 @@ contract BaseTest is Test {
                 && addr != address(0x4e59b44847b379578588920cA78FbF26c0B4956C)
                 && addr != address(0xb4c79daB8f259C7Aee6E5b2Aa729821864227e84)
                 && addr != address(0x185a4dc360CE69bDCceE33b3784B0282f7961aea)
-                && addr != address(0x7109709ECfa91a80626fF3989D68f67F5b1DD12D) && addr > address(0x9)
+                && addr != address(0x7109709ECfa91a80626fF3989D68f67F5b1DD12D) && addr > address(0xf)
         );
         assumePayable(addr);
     }
