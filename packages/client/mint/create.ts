654513db98028df7e84c0882c2e25bfe8a08fd2b
smallbraingames <smallbraindev@gmail.com>
2024-02-03 20:03:43 -0800

    feat(client): update create function to match new contract impl.

--- packages/client/mint/create.ts
+++ packages/client/mint/create.ts
@@ -1,14 +1,20 @@
-import { Address } from 'viem';
+import { WalletClient } from 'viem';
 
 import { getWriteCollectionContract } from './getCollectionContract';
 import getTransactionReceipt from './getTransactionReceipt';
-import ownerWalletClient from './ownerWalletClient';
 import publicClient from './publicClient';
 
-const create = async (creator: Address, supply: number) => {
-  const contract = getWriteCollectionContract(publicClient, ownerWalletClient);
+const create = async (walletClient: WalletClient, supply: number) => {
+  const contract = getWriteCollectionContract(publicClient, walletClient);
+  const creator = walletClient.account?.address;
+  if (!creator) {
+    throw Error('[Create] No address from wallet client');
+  }
   console.log(`[Create] Creating: (creator) ${creator}, (supply) ${supply}`);
-  const txHash = await contract.write.create([creator, supply]);
+  const pricePerSupply = (await contract.read.pricePerSupply()) as bigint;
+  const txHash = await contract.write.create([creator, supply], {
+    value: BigInt(supply) * pricePerSupply,
+  });
   const receipt = await getTransactionReceipt(txHash);
   if (receipt.status === 'reverted') {
     throw Error(
